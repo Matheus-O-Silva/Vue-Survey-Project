2a567572d2f5cd80276658261a1d9ff624bee5f3
Matheus Oliveira <matheus.oliveira110101@gmail.com>
2023-10-01 08:13:31 -0300

    adding isGuest meta

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,7 @@ const routes = [
         redirect: '/login',
         name: 'Auth',
         component: AuthLayout,
+        meta: {isGuest: true},
         children: [
             {
                 path:'/login',
@@ -54,7 +55,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
     if(to.meta.requiresAuth && !store.state.user.token){
         next({name: 'Login'})
-    } else if (store.state.user.token && (to.name === 'Login' || to.name === 'Register')){
+    } else if (store.state.user.token && (to.meta.isGuest)){
         next({name: 'Dashboard'})
     } else {
         next();
