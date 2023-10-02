2d1bc81ebbd49b5c11457f8d187663cfdda39d6f
Matheus Oliveira <matheus.oliveira110101@gmail.com>
2023-10-02 17:37:44 -0300

    PageComponent implementation

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,22 +1,9 @@
-<template lang="">
-  <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-      </div>
-    </header>
-    <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <div class="px-4 py-6 sm:px-0">
-          Content goes here
-        </div>
-      </div>
-    </main>
+<template>
+  <PageComponent title="Dashboard"> Content Goes Here </PageComponent>
 </template>
-<script>
-export default {
-  
-}
+
+<script setup>
+import PageComponent from "../components/PageComponent.vue";
 </script>
-<style lang="">
-  
-</style>
+
+<style></style>
