0619b5fccb73d300213fa49d55fdf3aa961e848c
Matheus Oliveira <matheus.oliveira110101@gmail.com>
2023-10-03 11:19:31 -0300

    add register form and confirmation password field

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,15 +2,15 @@
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
             alt="Your Company" />
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register</h2>
+        <h2 class="mt-3 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register</h2>
     </div>
 
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#" method="POST">
+    <div class=" sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" @submit="register">
             <div>
                 <label for="fullname" class="block text-sm font-medium leading-6 text-gray-900">Fullname</label>
                 <div class="mt-2">
-                    <input id="fullname" name="name" type="text" autocomplete="name" required="" placeholder="Fullname"
+                    <input id="fullname" name="name" type="text" autocomplete="name" required="" v-model="user.name"  placeholder="Fullname"
                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                 </div>
             </div>
@@ -18,7 +18,7 @@
             <div>
                 <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                 <div class="mt-2">
-                    <input id="email" name="email" type="email" autocomplete="email" required=""
+                    <input id="email" name="email" type="email" autocomplete="email" required="" v-model="user.email"  placeholder="Email Address"
                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                 </div>
             </div>
@@ -26,12 +26,19 @@
             <div>
                 <div class="flex items-center justify-between">
                     <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                    <div class="text-sm">
-                        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-                    </div>
                 </div>
                 <div class="mt-2">
-                    <input id="password" name="password" type="password" autocomplete="current-password" required=""
+                    <input id="password" name="password" type="password" autocomplete="current-password" required="" v-model="user.password"  placeholder="Password"
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+            </div>
+
+            <div>
+                <div class="flex items-center justify-between">
+                    <label for="password_confirmation" class="block text-sm font-medium leading-6 text-gray-900">Password Confirmation</label>
+                </div>
+                <div class="mt-2">
+                    <input id="password_confirmation" name="password_confirmation" type="password" autocomplete="password_confirmation" required="" v-model="user.password_confirmation"  placeholder="Password Confirmation"
                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                 </div>
             </div>
@@ -51,10 +58,25 @@
     </div>
 </template>
 
-<script>
-export default {
-    name: "Register"
+<script setup>
+import {LockClosedIcon} from '@heroicons/vue/solid'
+import store from "../store";
+import {useRouter} from "vue-router";
+const router = useRouter();
+const user = {
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
 }
+
+ev.preventDefault();
+store.dispatch('register',user).then(() => {
+    router.push({
+        name: 'Dashboard'
+    })
+});
+
 </script>
 
 <style>
